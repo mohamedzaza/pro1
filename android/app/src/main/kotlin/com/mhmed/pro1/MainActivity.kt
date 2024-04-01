52a07de73f771b5fdc9ebac908c68d1c3f193651
unknown <mohamedzaza>
2024-04-01 22:17:09 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/mhmed/pro1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mhmed.pro1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pro1/Home.dart';
+import 'package:pro1/school/Register.dart';
+import 'package:pro1/school/login.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        home:Car());
+
+    }
+}
